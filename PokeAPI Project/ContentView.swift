918a1445df5beb37662e627fe12f84ed88bdb817
Magdiel Borges <magdiel.borges@icloud.com>
2022-05-03 00:33:23 -0400

    Initial Commit

---
+++ PokeAPI Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PokeAPI Project
+//
+//  Created by Magdiel Borges on 5/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PokeAPI Project/PokeAPI_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeAPI_ProjectApp.swift
+//  PokeAPI Project
+//
+//  Created by Magdiel Borges on 5/3/22.
+//
+
+import SwiftUI
+
+@main
+struct PokeAPI_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
